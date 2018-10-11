94a4f17eec41b5a197fdbf52b0a678a4afd68b13
Eduard Popescu <peduard13@hotmail.com>
2018-10-11 21:09:53 +0300

    Refactor. Added IO monad.

---
+++ SoftwareCraft.Maybe/IO`1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SoftwareCraft.Maybe
+{
+	public struct Unit { }
+
+	// ReSharper disable once InconsistentNaming
+	public class IO<T>
+	{
+		private readonly Func<T> func;
+
+		public IO(Func<T> func) => this.func = func;
+
+		public IO<U> Bind<U>(Func<T, IO<U>> lift) => new IO<U>(() => lift(func()).Run());
+
+		public T Run() => func();
+	}
+}
--- SoftwareCraft.Maybe/Maybe`1.cs
+++ SoftwareCraft.Maybe/Maybe`1.cs
@@ -7,24 +7,9 @@ namespace SoftwareCraft.Maybe
 	{
 		protected T[] Items;
 
-		public abstract bool HasValue { get; }
-
-		public virtual void Map(Action<T> some) { }
-
 		public abstract void Map(Action<T> some, Action none);
 
 		public abstract U Map<U>(Func<T, U> some, Func<U> none);
-
-		public static Maybe<T> Wrap(Func<T> func)
-		{
-			if (func == null) throw new ArgumentNullException(nameof(func));
-
-			var result = func();
-
-			return result == null ?
-				(Maybe<T>) new None<T>() :
-				new Some<T>(result);
-		}
 
 		public Maybe<U> Bind<U>(Func<T, Maybe<U>> func)
 			=> Map(func, () => new None<U>());
@@ -41,16 +26,7 @@ namespace SoftwareCraft.Maybe
 			                                  value
 		                                  };
 
-		public override bool HasValue => true;
-
 		public T Value => Items[0];
-
-		public override void Map(Action<T> some)
-		{
-			if (some == null) throw new ArgumentNullException(nameof(some));
-
-			if (Items.Length != 0) some(Items[0]);
-		}
 
 		public override void Map(Action<T> some, Action none)
 		{
@@ -69,8 +45,6 @@ namespace SoftwareCraft.Maybe
 
 	public sealed class None<T> : Maybe<T>
 	{
-		public override bool HasValue => false;
-
 		public override void Map(Action<T> some, Action none)
 		{
 			if (none == null) throw new ArgumentNullException(nameof(none));
