93deb5af6ec8694d72b6fd29cf1f7d48be438fd6
Eduard Popescu <eduard.popescu@ailylabs.com>
2020-12-14 13:48:26 +0200

    Added helper methods to check that all maybes are either some or none; Implemented Lift extensions (execute a method on multiple maybe some values).

--- SoftwareCraft.Maybe/Maybe`1.cs
+++ SoftwareCraft.Maybe/Maybe`1.cs
@@ -1,77 +1,134 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SoftwareCraft.Functional
 {
-	public abstract class Maybe<T> : IEquatable<Maybe<T>>
-	{
-		protected readonly T[] Items;
+    public abstract class Maybe<T> : IEquatable<Maybe<T>>
+    {
+        protected readonly T[] Items;
 
-		protected Maybe() => Items = new T[0];
+        protected Maybe() => Items = new T[0];
 
-		protected Maybe(T value)
-		{
-			if (null == value) throw new ArgumentNullException();
+        protected Maybe(T value)
+        {
+            if (null == value) throw new ArgumentNullException();
 
-			Items = new[]
-			{
-				value
-			};
-		}
+            Items = new[]
+            {
+                value
+            };
+        }
 
-		public bool Equals(Maybe<T> other)
-		{
-			if (null == other) return false;
+        public abstract bool IsSome { get; }
 
-			switch (Items.Length)
-			{
-				case 1 when other.Items.Length == 0: // Some equals None?
-				case 0 when other.Items.Length == 1: // None equals Some?
-					return false;
-				case 0 when other.Items.Length == 0: // None equals None?
-					return true;
-				default:
-					return Items[0].Equals(other.Items[0]); // Some equals Some?
-			}
-		}
+        public abstract bool IsNone { get; }
 
-		public abstract bool IsSome { get; }
+        internal T Value
+        {
+            get
+            {
+                if (IsSome) return Items[0];
 
-		public abstract bool IsNone { get; }
+                throw new InvalidOperationException("Trying to call Value on a None.");
+            }
+        }
 
-		public abstract Maybe<U> Select<U>(Func<T, U> some, Func<U> none);
+        public bool Equals(Maybe<T> other)
+        {
+            if (null == other) return false;
 
-		public abstract Maybe<U> Select<U>(Func<T, U> some);
+            switch (Items.Length)
+            {
+                case 1 when other.Items.Length == 0: // Some equals None?
+                case 0 when other.Items.Length == 1: // None equals Some?
+                    return false;
+                case 0 when other.Items.Length == 0: // None equals None?
+                    return true;
+                default:
+                    return Items[0].Equals(other.Items[0]); // Some equals Some?
+            }
+        }
 
-		public abstract Maybe<U> SelectMany<U>(Func<T, Maybe<U>> some, Func<Maybe<U>> none);
+        public abstract Maybe<U> Select<U>(Func<T, U> some, Func<U> none);
 
-		public abstract Maybe<U> SelectMany<U>(Func<T, Maybe<U>> some);
+        public abstract Maybe<U> Select<U>(Func<T, U> some);
 
-		public abstract void Match(Action<T> some, Action none);
+        public abstract Maybe<U> SelectMany<U>(Func<T, Maybe<U>> some, Func<Maybe<U>> none);
 
-		public abstract TOut Match<TOut>(Func<T, TOut> some, Func<TOut> none);
+        public abstract Maybe<U> SelectMany<U>(Func<T, Maybe<U>> some);
 
-		public override bool Equals(object obj)
-		{
-			if (ReferenceEquals(null, obj)) return false;
-			if (ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != GetType()) return false;
-			return Equals((Maybe<T>)obj);
-		}
+        public abstract void Match(Action<T> some, Action none);
 
-		public static bool operator ==(Maybe<T> left, Maybe<T> right) => Equals(left, right);
+        public abstract TOut Match<TOut>(Func<T, TOut> some, Func<TOut> none);
 
-		public static bool operator !=(Maybe<T> left, Maybe<T> right) => !Equals(left, right);
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Maybe<T>) obj);
+        }
 
-		public override int GetHashCode() => Items.Length == 0 ? 0 : Items[0].GetHashCode();
-	}
+        public static bool operator ==(Maybe<T> left, Maybe<T> right) => Equals(left, right);
 
-	public static class MaybeExtensions
-	{
-		public static Maybe<U> Apply<T, U>(this Maybe<Func<T, U>> m, Maybe<T> other)
-			=> m.Match(f => other.Match(o => Maybe.Some(f(o)), Maybe.None<U>), Maybe.None<U>);
+        public static bool operator !=(Maybe<T> left, Maybe<T> right) => !Equals(left, right);
 
-		public static Maybe<U> Apply<T, U>(this Maybe<Func<T, U>> m, T other)
-			=> m.Match(f => Maybe.Some(f(other)), Maybe.None<U>);
-	}
+        public override int GetHashCode() => Items.Length == 0 ? 0 : Items[0].GetHashCode();
+    }
+
+    public static class MaybeExtensions
+    {
+        public static Maybe<T1> Apply<T, T1>(this Maybe<Func<T, T1>> m, Maybe<T> other)
+            => m.Match(f => other.Match(o => Maybe.Some(f(o)), Maybe.None<T1>), Maybe.None<T1>);
+
+        public static Maybe<T1> Apply<T, T1>(this Maybe<Func<T, T1>> m, T other)
+            => m.Match(f => Maybe.Some(f(other)), Maybe.None<T1>);
+
+        public static bool AllSome<T>(params Maybe<T>[] maybes)
+            => maybes.All(x => x.IsSome);
+
+        public static bool AnySome<T>(params Maybe<T>[] maybes)
+            => maybes.Any(x => x.IsSome);
+
+        public static bool AllNone<T>(params Maybe<T>[] maybes)
+            => maybes.All(x => x.IsNone);
+
+        public static bool AnyNone<T>(params Maybe<T>[] maybes)
+            => maybes.Any(x => x.IsNone);
+
+        public static Maybe<Tuple<T1, T2>> Lift<T1, T2>(Maybe<T1> m1, Maybe<T2> m2)
+        {
+            if (m1.IsSome && m2.IsSome)
+                return Maybe.Some(new Tuple<T1, T2>(m1.Value, m2.Value));
+
+            return Maybe.None<Tuple<T1, T2>>();
+        }
+
+        public static Maybe<Tuple<T1, T2, T3>> Lift<T1, T2, T3>(Maybe<T1> m1, Maybe<T2> m2, Maybe<T3> m3)
+        {
+            if (m1.IsSome && m2.IsSome && m3.IsSome)
+                return Maybe.Some(new Tuple<T1, T2, T3>(m1.Value, m2.Value, m3.Value));
+
+            return Maybe.None<Tuple<T1, T2, T3>>();
+        }
+
+        public static Maybe<Tuple<T1, T2, T3, T4>> Lift<T1, T2, T3, T4>(Maybe<T1> m1, Maybe<T2> m2, Maybe<T3> m3,
+            Maybe<T4> m4)
+        {
+            if (m1.IsSome && m2.IsSome && m3.IsSome && m4.IsSome)
+                return Maybe.Some(new Tuple<T1, T2, T3, T4>(m1.Value, m2.Value, m3.Value, m4.Value));
+
+            return Maybe.None<Tuple<T1, T2, T3, T4>>();
+        }
+
+        public static Maybe<Tuple<T1, T2, T3, T4, T5>> Lift<T1, T2, T3, T4, T5>(Maybe<T1> m1, Maybe<T2> m2, Maybe<T3> m3,
+            Maybe<T4> m4, Maybe<T5> m5)
+        {
+            if (m1.IsSome && m2.IsSome && m3.IsSome && m4.IsSome && m5.IsSome)
+                return Maybe.Some(new Tuple<T1, T2, T3, T4, T5>(m1.Value, m2.Value, m3.Value, m4.Value, m5.Value));
+
+            return Maybe.None<Tuple<T1, T2, T3, T4, T5>>();
+        }
+    }
 }
