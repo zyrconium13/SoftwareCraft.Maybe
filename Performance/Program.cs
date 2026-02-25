d15adf308e0d8b6c648205e97c1790a4cb15d046
Eduard Popescu <peduard13@hotmail.com>
2026-02-25 20:57:43 +0200

    lazy lift`2 tests and benchmarks

--- Performance/Program.cs
+++ Performance/Program.cs
@@ -2,10 +2,11 @@
 using BenchmarkDotNet.Running;
 using SoftwareCraft.Functional;
 
-var summary = BenchmarkRunner.Run<LiftingBenchmarks>();
+// BenchmarkRunner.Run<EagerLiftingBenchmarks>();
+BenchmarkRunner.Run<LazyLiftingBenchmarks>();
 
 [MemoryDiagnoser]
-public class LiftingBenchmarks
+public class EagerLiftingBenchmarks
 {
   private readonly Maybe<int>    m1 = Maybe.Some(13);
   private readonly Maybe<string> m2 = Maybe.Some("hello");
@@ -24,4 +25,26 @@ public class LiftingBenchmarks
 
   [Benchmark]
   public Maybe<Tuple<int, string>> EagerLift2D() => Maybe.Lifting.Lift(m3, m4);
+}
+
+[MemoryDiagnoser]
+public class LazyLiftingBenchmarks
+{
+  private readonly Func<Maybe<int>>    f1 = () => Maybe.Some(13);
+  private readonly Func<Maybe<string>> f2 = () => Maybe.Some("hello");
+
+  private readonly Func<Maybe<int>>    m3 = () => Maybe.None<int>();
+  private readonly Func<Maybe<string>> m4 = () => Maybe.None<string>();
+
+  [Benchmark]
+  public Maybe<Tuple<int, string>> LazyLift2A() => Maybe.Lifting.LiftLazy(f1, f2);
+
+  [Benchmark]
+  public Maybe<Tuple<int, string>> LazyLift2B() => Maybe.Lifting.LiftLazy(f1, m4);
+
+  [Benchmark]
+  public Maybe<Tuple<int, string>> LazyLift2C() => Maybe.Lifting.LiftLazy(m3, f2);
+
+  [Benchmark]
+  public Maybe<Tuple<int, string>> LazyLift2D() => Maybe.Lifting.LiftLazy(m3, m4);
 }
--- Tests/MaybeLifting2Tests.cs
+++ Tests/MaybeLifting2Tests.cs
@@ -6,6 +6,60 @@ using SoftwareCraft.Functional;
 [TestClass]
 public sealed class MaybeLifting2Tests
 {
+  #region Lazy Lift
+
+  [TestMethod]
+  public void LazyLiftTwoSome()
+  {
+    var f1 = () => Maybe.Some(13);
+    var f2 = () => Maybe.Some("hello");
+
+    var tuple = Maybe.Lifting.LiftLazy(f1, f2);
+
+    tuple.Match(
+      t =>
+      {
+        Assert.AreEqual(13,      t.Item1);
+        Assert.AreEqual("hello", t.Item2);
+      }
+    , () => Assert.Fail("This was not supposed to fail!"));
+  }
+
+  [TestMethod]
+  public void LazyLiftNoneAndSome()
+  {
+    var f1 = () => Maybe.None<int>();
+    var f2 = () => Maybe.Some("hello");
+
+    var tuple = Maybe.Lifting.LiftLazy(f1, f2);
+
+    Assert.IsTrue(tuple.IsNone);
+  }
+
+  [TestMethod]
+  public void LazyLiftSomeAndNone()
+  {
+    var f1 = () => Maybe.Some(13);
+    var f2 = () => Maybe.None<string>();
+
+    var tuple = Maybe.Lifting.LiftLazy(f1, f2);
+
+    Assert.IsTrue(tuple.IsNone);
+  }
+
+  [TestMethod]
+  public void LazyLiftTwoNone()
+  {
+    var f1 = () => Maybe.None<int>();
+    var f2 = () => Maybe.None<string>();
+
+    var tuple = Maybe.Lifting.LiftLazy(f1, f2);
+
+    Assert.IsTrue(tuple.IsNone);
+  }
+
+  #endregion
+
   #region Eager Lift
 
   [TestMethod]
